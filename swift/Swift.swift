ef8dda823af730f7e9b35134f1743b656f53a54c
boriskol <kolaricb@gmail.com>
2020-02-26 09:43:23 -0500

    swift

--- swift/Swift.swift
+++ swift/Swift.swift
@@ -222,3 +222,19 @@ DispatchQueue.global(qos: .default).async {
         self.alert.text = "Waiting over"
     }
 }
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0..<Swift.min($0 + size, count)])
+        }
+    }
+}
+extension Array where Element: Equatable {
+    // Remove first collection element that is equal to the given `object`:
+    mutating func remove(object: Element) {
+        guard let index = firstIndex(of: object) else {return}
+        remove(at: index)
+    }
+
+}
